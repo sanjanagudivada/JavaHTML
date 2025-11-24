84b9b8c3e13083d6d63090feb1033b96ef1e168f
sanjanagudivada <sanjana.gudivada@msds.christuniversity.in>
2025-11-24 12:51:04 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,31 +1,23 @@
 async function searchCity() {
     const city = document.getElementById("city").value;
-    const API_KEY = "d9c1431e6b7eb69ff0324092df2895b9";  // Replace
-
+    const API_KEY = "d9c1431e6b7eb69ff0324092df2895b9";  // Replac
     document.getElementById("output").innerText = "Loading...";
-
     // Step 1: Get latitude + longitude
     const geoURL =
         `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
-
     const geoRes = await fetch(geoURL);
     const geoData = await geoRes.json();
-
     if (geoData.length === 0) {
         document.getElementById("output").innerText = "City not found!";
         return;
     }
-
     const lat = geoData[0].lat;
     const lon = geoData[0].lon;
-
     // Step 2: Get weather using coordinates
     const weatherURL =
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
-
     const weatherRes = await fetch(weatherURL);
     const weatherData = await weatherRes.json();
-
     // Display result
     document.getElementById("output").innerText =
         `City: ${weatherData.name}\n` +
